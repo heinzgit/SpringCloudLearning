d01a12bb1bcce198006566e3ca4e17fbcfab824e
heinzgit <heinzfei@163.com>
2021-10-24 22:15:14 +0800

    Service B

---
+++ ServiceB/src/main/java/com/example/demo/EntryController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableDiscoveryClient
+public class EntryController {
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/hello")
+	public String hello() {
+		return "Service B";
+	}
+
+}
